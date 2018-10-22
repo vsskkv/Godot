4be423b2e03b7bfb07ffd0e6a12bec8c8bb8c492
vsskkv <vskkv@gmail.com>
2018-10-22 14:26:32 +0100

    notes

--- simple platform game/WorldComplete.gd
+++ simple platform game/WorldComplete.gd
@@ -8,5 +8,6 @@ func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 
 	for body in bodies:
-		if body.name == Player:
+		if (body.name == get_node("../Player").name):
+			print("next world");
 			get_tree().change_scene(worldScene)
